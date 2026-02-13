294ba1c7ef01a0b705789e30f298040284b811ed
gitdewlab <dewlab@proton.me>
2026-02-13 18:08:29 +0530

    Update main.py

--- dew/main.py
+++ dew/main.py
@@ -46,8 +46,8 @@ if wlan.isconnected():
     try:
         ntptime.settime()
         print("Time synchronized with NTP server")
-    except OSError:
-        print("Failed to synchronize time via NTP")
+    except OSError as e:
+        print(f"error: {e}")
 
 def ntp_update(timer):
     global ntp_update_due
@@ -58,7 +58,7 @@ def screen_update(timer):
     screen_update_due = True
     
 screen_timer.init(mode=machine.Timer.PERIODIC, period=500, callback=screen_update)
-ntp_timer.init(mode=machine.Timer.PERIODIC, period=3600000, callback=ntp_update)
+ntp_timer.init(mode=machine.Timer.PERIODIC, period=900000, callback=ntp_update)
 
 def get_local_time(offset_seconds):
     utc_seconds = time.time()
@@ -83,8 +83,8 @@ while True:
             try:
                 ntptime.settime()
                 print("Time synchronized with NTP server")
-            except OSError:
-                print("Failed to synchronize time via NTP")
+            except OSError as e:
+                print(f"error: {e}")
         display.clear()
         display.text("SYNC")
         display.show()
