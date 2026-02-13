d56ed3a884e8867b0fa3ba7fd12ff7e7e0ec6aec
gitdewlab <dewlab@proton.me>
2026-02-14 01:57:05 +0530

    Update main.py

--- dew/main.py
+++ dew/main.py
@@ -60,7 +60,7 @@ def screen_update(timer):
     screen_update_due = True
     
 screen_timer.init(mode=machine.Timer.PERIODIC, period=500, callback=screen_update)
-ntp_timer.init(mode=machine.Timer.PERIODIC, period=10000, callback=ntp_update)
+ntp_timer.init(mode=machine.Timer.PERIODIC, period=900000, callback=ntp_update)
 
 def get_local_time(offset_seconds):
     utc_seconds = time.time()
