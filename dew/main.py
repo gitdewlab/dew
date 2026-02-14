1957ae9479b4d62226c449c26ff12ab39ab8dc8b
gitdewlab <dewlab@proton.me>
2026-02-15 00:55:30 +0530

    Update main.py

--- dew/main.py
+++ dew/main.py
@@ -45,6 +45,39 @@ def demo(np):
         np.write()
         time.sleep_ms(60)
 
+        # bounce
+    for i in range(4 * n):
+        for j in range(n):
+            np[j] = (0, 128, 128)
+        if (i // n) % 2 == 0:
+            np[i % n] = (0, 0, 0)
+        else:
+            np[n - 1 - (i % n)] = (0, 0, 0)
+        np.write()
+        time.sleep_ms(60)
+
+    # bounce
+    for i in range(4 * n):
+        for j in range(n):
+            np[j] = (128, 128, 0)
+        if (i // n) % 2 == 0:
+            np[i % n] = (0, 0, 0)
+        else:
+            np[n - 1 - (i % n)] = (0, 0, 0)
+        np.write()
+        time.sleep_ms(60)
+
+        # bounce
+    for i in range(4 * n):
+        for j in range(n):
+            np[j] = (128, 0, 128)
+        if (i // n) % 2 == 0:
+            np[i % n] = (0, 0, 0)
+        else:
+            np[n - 1 - (i % n)] = (0, 0, 0)
+        np.write()
+        time.sleep_ms(60)
+
     # fade in/out
     for i in range(0, 4 * 256, 8):
         for j in range(n):
@@ -75,6 +108,39 @@ def demo(np):
             else:
                 val = 255 - (i & 0xff)
             np[j] = (0, 0, val)
+        np.write()
+        time.sleep_ms(35)
+
+        # fade in/out
+    for i in range(0, 4 * 256, 8):
+        for j in range(n):
+            if (i // 256) % 2 == 0:
+                val = i & 0xff
+            else:
+                val = 255 - (i & 0xff)
+            np[j] = (val, val, 0)
+        np.write()
+        time.sleep_ms(35)
+
+        # fade in/out
+    for i in range(0, 4 * 256, 8):
+        for j in range(n):
+            if (i // 256) % 2 == 0:
+                val = i & 0xff
+            else:
+                val = 255 - (i & 0xff)
+            np[j] = (0, val, val)
+        np.write()
+        time.sleep_ms(35)
+
+        # fade in/out
+    for i in range(0, 4 * 256, 8):
+        for j in range(n):
+            if (i // 256) % 2 == 0:
+                val = i & 0xff
+            else:
+                val = 255 - (i & 0xff)
+            np[j] = (val, 0, val)
         np.write()
         time.sleep_ms(35)
 
